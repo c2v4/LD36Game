05d57180e12c7f2646fd38ae6265200784447aa8
Mateusz <mateusz.duda.md@gmail.com>
2016-08-27 12:51:21 +0200

    Added dispatching people to jobs

--- src/modules/application/angular/components/tribe/Tribe.ts
+++ src/modules/application/angular/components/tribe/Tribe.ts
@@ -10,6 +10,10 @@ class TribeController {
 
     static $inject: Array<string> = ['$interval'];
 
+    constructor(public $interval: IIntervalService) {
+        $interval(this.tick, this.tickTime);
+    }
+
     public tickTime = 1000;
     public resources: Resources = {
         food: 100
@@ -22,20 +26,35 @@ class TribeController {
         {
             cardinality: 16,
             profession: {
-                name: 'idle', foodConsumption: 0.1, act: ()=> {
+                name: 'idle', foodConsumption: 0.1, efficiency: 0, act: (efficiency)=> {
                 }
             }
         },
         {
             cardinality: 0,
             profession: {
-                name: 'hunter', foodConsumption: 0.3, act: ()=> {
+                name: 'hunter',
+                foodConsumption: 0.3,
+                efficiency: 0.5,
+                act: (efficiency)=> {
+                    this.resources.food += efficiency;
+
                 }
             }
         },
     ];
+
+    public availableWorkers(): boolean {
+        return _(this.population).filter((item: PopulationEntry)=> {
+                return item.profession.name == "idle";
+            }).map((item: PopulationEntry)=> {
+                return item.cardinality
+            }).value() > 0;
+    }
+
     public tick: Function = ()=> {
         this.feed();
+        this.work();
     };
     public feed: Function = ()=> {
         this.resources.food -= _(this.population).map((item: PopulationEntry)=> {
@@ -43,11 +62,29 @@ class TribeController {
         }).sum();
     };
 
-    constructor(public $interval: IIntervalService) {
-        $interval(this.tick, this.tickTime);
+    private work() {
+        this.population.forEach((entry: PopulationEntry)=> {
+            entry.profession.act(entry.profession.efficiency * entry.cardinality);
+        })
     }
 
+    public addWorker(worker: PopulationEntry): void {
+        _(this.population).filter((item: PopulationEntry)=> {
+            return item.profession.name == "idle";
+        }).forEach((item: PopulationEntry)=> {
+            item.cardinality--
+        });
+        worker.cardinality++;
+    }
 
+    public removeWorker(worker: PopulationEntry): void {
+        _(this.population).filter((item: PopulationEntry)=> {
+            return item.profession.name == "idle";
+        }).forEach((item: PopulationEntry)=> {
+            item.cardinality++
+        });
+        worker.cardinality--;
+    }
 }
 interface Resources {
     food: number
@@ -57,7 +94,8 @@ interface Resources {
 // }
 interface Profession {
     name: string,
-    foodConsumption: number
+    foodConsumption: number,
+    efficiency: number,
     act: Function
 }
 interface PopulationEntry {
