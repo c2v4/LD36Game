a1b1a723635d29cb6823099a5c21fd98e4aed834
Mateusz <mateusz.duda.md@gmail.com>
2016-08-27 11:06:11 +0200

    Added population

--- src/modules/application/angular/components/tribe/Tribe.ts
+++ src/modules/application/angular/components/tribe/Tribe.ts
@@ -1,3 +1,44 @@
-export class Tribe implements ng.IComponentOptions{
+import IIntervalService = angular.IIntervalService;
+export class Tribe implements ng.IComponentOptions {
+    public controller: Function = TribeController;
     public template: string = require("./tribe-template.html");
+    public controllerAs: string = "vm"
+}
+class TribeController {
+
+    static $inject: Array<string> = ['$interval'];
+
+    public population: number = 16;
+    public tickTime = 1000;
+    public availableProfessions: Array<Profession> = [
+        {
+            job: "idle",
+            foodConsumption: 0.1
+        }
+    ];
+    public tick: Function = ()=> {
+        this.population--;
+    };
+    public feed: Function = ()=> {
+
+    };
+    public resources: Resources = {
+        food: 100
+    };
+
+    constructor(public $interval: IIntervalService) {
+        $interval(this.tick, this.tickTime);
+    }
+
+
+}
+interface Resources {
+    food: number
+}
+interface Population {
+    idle: number;
+}
+interface Profession {
+    job: string,
+    foodConsumption: number
 }
