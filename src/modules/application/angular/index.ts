0cae0a5a185c93aab6012680eb4f4dce8cec9546
Mateusz <mateusz.duda.md@gmail.com>
2016-08-28 15:15:54 +0200

    Removed unused library

--- src/modules/application/angular/index.ts
+++ src/modules/application/angular/index.ts
@@ -1,5 +1,5 @@
 import "angular";
 import {Tribe} from "./components/tribe/Tribe";
 
-angular.module("app.application", ['ngMaterial', 'ngMdIcons'])
+angular.module("app.application", ['ngMaterial'])
     .component("tribe", new Tribe());
